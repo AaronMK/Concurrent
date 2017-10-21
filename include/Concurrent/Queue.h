52ca48c5ff70cb87d52f48ba49373a14dc061d4a
Aaron Karp <aaron.m.karp@gmail.com>
2017-10-21 13:19:10 -0400

    Queue supports de-queuing into a std::optional.

--- include/Concurrent/Queue.h
+++ include/Concurrent/Queue.h
@@ -12,8 +12,11 @@ namespace Concurrent
 	 *  Queue with a lock-free implementation on supported platforms.
 	 */
 	template<typename T>
-	class Queue : public QueuePlatform<T>
+	class Queue
 	{
+	private:
+		QueuePlatform<std::optional<T>> mSysQueue;
+
 	public:
 
 		/**
@@ -43,6 +46,14 @@ namespace Concurrent
 		 *  Otherwise, destination remains unchanged and false is returned.
 		 */
 		bool tryPop(T& destination);
+		
+		/**
+		 * @brief
+		 *  Attempts to pop an item from the Queue, if there is something to
+		 *  de-queue, it is placed in destination and true is returned.
+		 *  Otherwise, destination remains unchanged and false is returned.
+		 */
+		bool tryPop(std::optional<T>& destination);
 
 		/**
 		 * @brief
@@ -66,17 +77,30 @@ namespace Concurrent
 	template<typename T>
 	void Queue<T>::push(const T& item)
 	{
-		mSysQueue.push(item);
+		mSysQueue.push(std::make_optional<T>(item));
 	}
 
 	template<typename T>
 	void Queue<T>::push(T&& item)
 	{
-		mSysQueue.push(std::forward<T>(item));
+		mSysQueue.push(std::make_optional<T>(std::move(item)));
 	}
 
 	template<typename T>
 	bool Queue<T>::tryPop(T& destination)
+	{
+		std::optional<T> temp;
+		if (mSysQueue.try_pop(temp))
+		{
+			destination = std::move(*temp);
+			return true;
+		}
+
+		return false;
+	}
+
+	template<typename T>
+	bool Queue<T>::tryPop(std::optional<T>& destination)
 	{
 		return mSysQueue.try_pop(destination);
 	}
