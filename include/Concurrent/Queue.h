106785e62be91df1b4118e9489e6e97e3f5324e5
Aaron Karp <aaron.m.karp@gmail.com>
2017-10-23 01:16:52 -0400

    Exception thrown on attempts by Queue to copy or more a type that does not support the respective operation.

--- include/Concurrent/Queue.h
+++ include/Concurrent/Queue.h
@@ -2,9 +2,9 @@
 #define _CONCURRENT_QUEUE_H_
 
 #include "Config.h"
-
 #include "Internal/QueuePlatform.h"
 
+#include <StdExt/Exceptions.h>
 #include <optional>
 
 namespace Concurrent
@@ -19,27 +19,67 @@ namespace Concurrent
 	private:
 		QueuePlatform<std::optional<T>> mSysQueue;
 
+		template<bool canCopy>
+		void copyPush(const T& item);
+
+		template<>
+		void copyPush<true>(const T& item)
+		{
+			mSysQueue.push(std::make_optional<T>(item));
+		}
+		
+		template<>
+		void copyPush<false>(const T& item)
+		{
+			throw StdExt::invalid_operation("Attempting to copy a non-copyable object.")
+		}
+		
+		template<bool canMove>
+		void movePush(T&& item);
+
+		template<>
+		void movePush<true>(T&& item)
+		{
+			mSysQueue.push(std::make_optional<T>(std::move(item)));
+		}
+		
+		template<>
+		void movePush<false>(T&& item)
+		{
+			throw StdExt::invalid_operation("Attempting to move a non-movable object.")
+		}
+
 	public:
 
 		/**
 		 * @brief
 		 *  Creates an empty queue.
 		 */
-		Queue();
+		Queue()
+		{
+		}
 
-		virtual ~Queue();
-
-		/**
-		 * @brief
-		 *  Pushes an item onto the Queue.
-		 */
-		void push(const T& item);
+		virtual ~Queue()
+		{
+		}
 
 		/**
 		 * @brief
 		 *  Pushes an item onto the Queue.
 		 */
-		void push(T&& item);
+		void push(const T& item)
+		{
+			copyPush<std::is_copy_constructible<T>::value>(item);
+		}
+
+		/**
+		 * @brief
+		 *  Pushes an item onto the Queue.
+		 */
+		void push(T&& item)
+		{
+			movePush<std::is_copy_constructible<T>::value>(std::move(item));
+		}
 
 		/**
 		 * @brief
@@ -47,73 +87,38 @@ namespace Concurrent
 		 *  de-queue, it is placed in destination and true is returned.
 		 *  Otherwise, destination remains unchanged and false is returned.
 		 */
-		bool tryPop(T& destination);
-		
+		bool tryPop(T& destination)
+		{
+			std::optional<T> temp;
+			if (mSysQueue.try_pop(temp))
+			{
+				destination = std::move(*temp);
+				return true;
+			}
+
+			return false;
+		}
+
 		/**
 		 * @brief
 		 *  Attempts to pop an item from the Queue, if there is something to
 		 *  de-queue, it is placed in destination and true is returned.
 		 *  Otherwise, destination remains unchanged and false is returned.
 		 */
-		bool tryPop(std::optional<T>& destination);
+		bool tryPop(std::optional<T>& destination)
+		{
+			return mSysQueue.try_pop(destination);
+		}
 
 		/**
 		 * @brief
 		 *  Inspector to determine if the queue is empty.
 		 */
-		bool isEmpty() const;
-	};
-
-#ifdef _WIN32
-
-	template<typename T>
-	Queue<T>::Queue()
-	{
-	}
-
-	template<typename T>
-	Queue<T>::~Queue()
-	{
-	}
-
-	template<typename T>
-	void Queue<T>::push(const T& item)
-	{
-		mSysQueue.push(std::make_optional<T>(item));
-	}
-
-	template<typename T>
-	void Queue<T>::push(T&& item)
-	{
-		mSysQueue.push(std::make_optional<T>(std::move(item)));
-	}
-
-	template<typename T>
-	bool Queue<T>::tryPop(T& destination)
-	{
-		std::optional<T> temp;
-		if (mSysQueue.try_pop(temp))
+		bool isEmpty() const
 		{
-			destination = std::move(*temp);
-			return true;
+			return mSysQueue.empty();
 		}
-
-		return false;
-	}
-
-	template<typename T>
-	bool Queue<T>::tryPop(std::optional<T>& destination)
-	{
-		return mSysQueue.try_pop(destination);
-	}
-
-	template<typename T>
-	bool Queue<T>::isEmpty() const
-	{
-		return mSysQueue.empty();
-	}
-
-#endif
+	};
 }
 
 #endif _CONCURRENT_QUEUE_H_
